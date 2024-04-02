0afc455158ac5e6636309d96d64ef3115e8b1331
Ahsan855 <md.ahsanhabib9869@gmail.com>
2024-04-02 11:47:04 +0600

    added login page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00FFB9",
+        secondary: "#0F111B",
+      },
+    },
   },
   plugins: [],
 };

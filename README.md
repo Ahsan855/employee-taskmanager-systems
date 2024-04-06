71e7c9c585f9bd65f3b6f495299bbb87948fa515
Ahsan855 <md.ahsanhabib9869@gmail.com>
2024-04-06 08:25:23 +0600

    added md file

--- README.md
+++ README.md
@@ -1,8 +1,59 @@
-# React + Vite
+# Task Management System Documentation
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Admin Features:
 
-Currently, two official plugins are available:
+### User Management:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- Create admin accounts.
+- Add and manage team members.
+
+### Task Assignment:
+
+- Assign tasks to individual or multiple users.
+- Update task details and status.
+
+### Task Properties:
+
+- Label tasks as todo, in progress, or completed.
+- Assign priority levels (high, medium, normal, low).
+- Add and manage sub-tasks.
+
+### Asset Management:
+
+- Upload task assets, such as images.
+
+### User Account Control:
+
+- Disable or activate user accounts.
+- Permanently delete or trash tasks.
+
+## User Features:
+
+### Task Interaction:
+
+- Change task status (in progress or completed).
+- View detailed task information.
+
+### Communication:
+
+- Add comments or chat to task activities.
+
+## General Features:
+
+### Authentication and Authorization:
+
+- User login with secure authentication.
+- Role-based access control.
+
+### Profile Management:
+
+- Update user profiles.
+
+### Password Management:
+
+- Change passwords securely.
+
+### Dashboard:
+
+- Provide a summary of user activities.
+- Filter tasks into todo, in progress, or completed.
--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,26 +1,51 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ModalWrapper from "./ModalWrapper";
 import { Dialog } from "@headlessui/react";
 import Textbox from "./Textbox";
 import Loading from "./Loader";
 import Button from "./Button";
+import { useRegisterMutation } from "../redux/slices/api/authApiSlice";
+import { toast } from "sonner";
+import { useUpdateUserMutation } from "../redux/slices/api/userApiSlice";
+import { setCredentials } from "../redux/slices/authSlice";
 
 const AddUser = ({ open, setOpen, userData }) => {
   let defaultValues = userData ?? {};
   const { user } = useSelector((state) => state.auth);
-
-  const isLoading = false,
-    isUpdating = false;
 
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({ defaultValues });
+  const dispatch = useDispatch();
+  const [addNewUser, { isLoading }] = useRegisterMutation();
+  const [updateUser, { isLoading: isUpdating }] = useUpdateUserMutation();
+  const handleOnSubmit = async (data) => {
+    try {
+      if (userData) {
+        const result = await updateUser(data).unwrap();
 
-  const handleOnSubmit = () => {};
+        toast.success(result?.message);
+        if (userData?._id === user > _id) {
+          dispatch(setCredentials({ ...result.user }));
+        }
+      } else {
+        await addNewUser({
+          ...data,
+          password: data.email,
+        }).unwrap();
+        toast.success("New User added successfully");
+      }
+      setTimeout(() => {
+        setOpen(false);
+      }, 1500);
+    } catch (err) {
+      toast.error("Something went wrong");
+    }
+  };
 
   return (
     <>
